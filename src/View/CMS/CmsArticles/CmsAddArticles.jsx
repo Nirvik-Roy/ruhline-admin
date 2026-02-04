b1958823a9e8f08a7f326bfded447c0674bd2f8f
Nirvik Roy <nirvikroy802@gmail.com>
2026-02-04 18:59:19 +0530

    article section frontend part done

--- src/View/CMS/CmsArticles/CmsAddArticles.jsx
+++ src/View/CMS/CmsArticles/CmsAddArticles.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react'
+import React, { lazy, Suspense, useState } from 'react'
 import Button from '../../../Components/Button'
 import Input from '../../../Components/Input'
 import CustomTextEditor from '../../../Components/CustomTextEditor/CustomTextEditor'
@@ -8,20 +8,41 @@ import { useNavigate } from 'react-router-dom'
 const CmsAddArticles = () => {
     const [isModal, setisModal] = useState(false);
     const navigate = useNavigate();
-    const formStructure = [
-        {
-            id:1,
-            name:true,
-            blogCategory:true,
-            description:true,
-            uploadImage:true,
-            shareOption:true,
-        },
-    ]
+    const [dynamicFormstructure, setdynamicformstructure] = useState([]);
+    const [textAreaContent, settextareacontent] = useState()
+
+    const deleteFunc = (id) => {
+        const dummyData = [...dynamicFormstructure];
+        const filteredData = dummyData.filter((e) => e.id != id);
+        setdynamicformstructure(filteredData)
+    }
+    const handleChange = (id, e) => {
+        const { name, value } = e.target;
+        setdynamicformstructure(prevItems =>
+            prevItems.map(item =>
+                item.id === id ? { ...item, [name]: value } : item
+            )
+        );
+    }
+    const handleImage = (id, e) => {
+        setdynamicformstructure(prevItems => (
+            prevItems.map(item =>
+                item.id === id ? { ...item, ['uploadImage']: e.target.files[0] } : item
+            )
+        ))
+    }
+    const ontextChange = (id, data) => {
+        settextareacontent(data)
+        setdynamicformstructure(prevItems => (
+            prevItems.map(item => item.id === id ? { ...item, ['description']: data } : item)
+        ))
+    }
+
+    console.log(dynamicFormstructure)
     return (
         <>
 
-            {isModal && <Addarticlesectionmodal setisModal={setisModal} />}
+            {isModal && <Addarticlesectionmodal setdynamicformstructure={setdynamicformstructure} dynamicFormstructure={dynamicFormstructure} setisModal={setisModal} />}
 
             <div className='dashboard_container'>
                 <div className='coaches_head_wrapper'>
@@ -47,7 +68,7 @@ const CmsAddArticles = () => {
                         </div>
                     </div>
                 </div>
-                <form onSubmit={((e)=>e.preventDefault())}>
+                <form>
                     <div className='add_articles_wrapper'>
                         <div className='articles_form_left'>
                             <Input label={'Blog Name'} required={true} placeholder={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
@@ -60,6 +81,22 @@ const CmsAddArticles = () => {
                             </div>
 
                             <CustomTextEditor label={'Description'} required={true} />
+
+                        </div>
+                        <div className='articles_form_left'>
+                            <div className='input_form'>
+                                <label style={{
+                                    fontSize: '15px',
+                                }}>Upload Thumbnail Image<span>*</span></label>
+
+                                <div className='files_upload_wrapper'>
+                                    <img src={upload} />
+                                    <p>Drag your files or <span>Browse</span></p>
+                                    <h5>Png, Jpg, Jpeg supported | file size: 250 KB</h5>
+                                    <input type='file' />
+                                </div>
+                            </div>
+
                             <div className='share_post_wrapper'>
                                 <p>Share the post option</p>
                                 <div className='share_checkbox_Wrapper'>
@@ -78,89 +115,87 @@ const CmsAddArticles = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='articles_form_left'>
-                            <div className='input_form'>
-                                <label style={{
-                                    fontSize: '15px',
-                                }}>Upload Thumbnail Image<span>*</span></label>
-
-                                <div className='files_upload_wrapper'>
-                                    <img src={upload} />
-                                    <p>Drag your files or <span>Browse</span></p>
-                                    <h5>Png, Jpg, Jpeg supported | file size: 250 KB</h5>
-                                    <input type='file' />
-                                </div>
-                            </div>
-
-                           
-                        </div>
                     </div>
-
-
-                    <div className='article_section_wrapper'>
-                        <div className='article_section_head_wrapper'>
-                            <h2>Section 1</h2>
-                            <i class="fa-solid fa-xmark"></i>
-                        </div>
-                        <div className='add_articles_wrapper'>
-                            <div className='articles_form_left'>
-                                <Input label={'Blog Name'} required={true} placeholder={'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'} />
-
-                                <div className='input_form'>
-                                    <label>Select Blog Category <span>*</span></label>
-                                    <select>
-                                        <option>Blog Category 1</option>
-                                    </select>
-                                </div>
-
-                                <CustomTextEditor label={'Description'} required={true} />
-
+                    {dynamicFormstructure?.map((e, i) => (
+                        <div key={e?.id} className='article_section_wrapper'>
+                            <div className='article_section_head_wrapper'>
+                                <h2>Section {i + 1}</h2>
+                                <i onClick={(() => { deleteFunc(e?.id) })} class="fa-solid fa-xmark"></i>
                             </div>
-                            <div className='articles_form_left'>
-                                <div className='input_form'>
-                                    <div className='input_label_wrapper462'>
-                                        <label style={{
-                                            fontSize: '15px',
-                                        }}>Upload Thumbnail Image<span>*</span></label>
-                                        <div className='image_position_wrapper'>
-                                            <p>Place image on</p>
-                                            <select>
-                                                <option>Right Side</option>
-                                                <option>Left Side</option>
-                                            </select>
+                            <div className='add_articles_wrapper'>
+                                <div className='articles_form_left'>
+                                    {(e?.heading != null) && <div>
+                                        <Input name={'heading'} onChange={((ele) => { handleChange(e?.id, ele) })} label={'Heading'} required={true} placeholder={'Enter blog heading..'} />
+                                    </div>}
+
+                                    {(e?.buttonName != null && (e?.buttonUrl != null)) && <div className='modal_input_grid_wrapper'>
+                                        <div>
+                                            <Input name={'buttonName'} onChange={((ele) => { handleChange(e?.id, ele) })} label={'Button Name'} required={true} placeholder={'Learn More'} />
+                                        </div>
+                                        <div>
+                                            <Input name={'buttonUrl'} onChange={((ele) => { handleChange(e?.id, ele) })} label={'Button Url'} required={true} placeholder={'www.website.com'} />
                                         </div>
 
-                                    </div>
+                                    </div>}
+
+                                    {(e?.description != null) && <div>
+                                        <CustomTextEditor onChange={((data) => ontextChange(e?.id, data))} label={'Description'} required={true} />
+
+                                    </div>}
+
+                                </div>
+                                <div className='articles_form_left'>
+                                    {(e?.uploadImage!= null) && <div className='input_form'>
+                                        <div className='input_label_wrapper462'>
+                                            <label style={{
+                                                fontSize: '15px',
+                                            }}>Upload Thumbnail Image<span>*</span></label>
+                                            <div className='image_position_wrapper'>
+                                                <p>Place image on</p>
+                                                <select>
+                                                    <option>Left Side</option>
+                                                    <option>Right Side</option>
+
+                                                </select>
+                                            </div>
+
+                                        </div>
 
 
-                                    <div className='files_upload_wrapper'>
-                                        <img src={upload} />
-                                        <p>Drag your files or <span>Browse</span></p>
-                                        <h5>Png, Jpg, Jpeg supported | file size: 250 KB</h5>
-                                        <input type='file' />
-                                    </div>
+                                        <div className='files_upload_wrapper'>
+                                            {(e?.uploadImage == '') && <>
+                                                <img src={upload} />
+                                                <p>Drag your files or <span>Browse</span></p>
+                                                <h5>Png, Jpg, Jpeg supported | file size: 250 KB</h5>
+                                            </>}
+
+                                            {(e?.uploadImage != '') && <>
+                                                <img src={URL.createObjectURL(e?.uploadImage)} style={{
+                                                    width: '100%',
+                                                    objectFit: 'contain',
+                                                    height: '95%'
+                                                }} />
+                                            </>}
+                                            <input onChange={((ele) => { handleImage(e?.id, ele) })} type='file' />
+                                        </div>
+                                    </div>}
                                 </div>
                             </div>
                         </div>
-                    </div>
-
-
-
-                   
-
+                    ))}
 
 
                     {/* <div className='section_divider'></div> */}
                     <div onClick={(() => setisModal(!isModal))}>
 
-                    <Button children={'Add Section'} styles={{
-                        color: 'var(--text-color)',
-                        border: '1px solid var(--primary-color)',
-                        padding: '12px 15px',
-                        background: 'transparent',
-                        fontSize: '13px',
-                        marginTop: '30px'
-                    }} />
+                        <Button children={'Add Section'} styles={{
+                            color: 'var(--text-color)',
+                            border: '1px solid var(--primary-color)',
+                            padding: '12px 15px',
+                            background: 'transparent',
+                            fontSize: '13px',
+                            marginTop: '30px'
+                        }} />
                     </div>
 
                 </form>
--- src/View/Modal/Addarticlesectionmodal.jsx
+++ src/View/Modal/Addarticlesectionmodal.jsx
@@ -1,37 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../Components/Button.jsx'
-const Addarticlesectionmodal = ({setisModal}) => {
+import toast from 'react-hot-toast';
+const Addarticlesectionmodal = ({ setisModal, setdynamicformstructure, dynamicFormstructure }) => {
+
+    const [imageCheck, setimageCheck] = useState(false);
+    const [headingCheck, setheadingCheck] = useState(false);
+    const [descriptioncheck, setdescriptionCheck] = useState(false);
+    const [buttonCheck, setbuttonCheck] = useState(false);
+
+    const addFunction = () => {
+        if (imageCheck || headingCheck || descriptioncheck || buttonCheck) {
+            const data = {}
+            data.id = Date.now()
+            data.section = `Section ${(dynamicFormstructure?.length) +1}`
+            if (imageCheck) {
+                data.uploadImage = ''
+            }
+            if (headingCheck) {
+                data.heading = ''
+            }
+            if (descriptioncheck) {
+                data.description = ''
+            }
+            if (buttonCheck) {
+                data.buttonName = '';
+                data.buttonUrl = '';
+            }
+            setdynamicformstructure([
+                ...dynamicFormstructure,
+                data
+            ])
+            setisModal(false)
+        } else {
+            toast.error('You need select atleast one option...')
+        }
+    }
     return (
         <>
             <div className='modal_wrapper' onClick={(() => setisModal(false))}></div>
             <div className='modal_div'>
-            <h4>Add Section</h4>
+                <h4>Add Section</h4>
                 <i class="fa-solid fa-xmark" onClick={(() => setisModal(false))}></i>
                 <div className='add_section_checkbox_Wrapper466'>
                     <div className='article_section_checkbox'>
-                        <input type='checkbox'/>
+                        <input onChange={(() => setimageCheck(!imageCheck))} checked={imageCheck} type='checkbox' />
                         <p>Image</p>
                     </div>
 
-                     <div className='article_section_checkbox'>
-                        <input type='checkbox'/>
+                    <div className='article_section_checkbox'>
+                        <input type='checkbox' onChange={(() => setheadingCheck(!headingCheck))} checked={headingCheck} />
                         <p>Heading</p>
                     </div>
 
-                     <div className='article_section_checkbox'>
-                        <input type='checkbox'/>
+                    <div className='article_section_checkbox'>
+                        <input onChange={(() => setdescriptionCheck(!descriptioncheck))} checked={descriptioncheck} type='checkbox' />
                         <p>Description</p>
                     </div>
 
-                     <div className='article_section_checkbox'>
-                        <input type='checkbox'/>
+                    <div className='article_section_checkbox'>
+                        <input onChange={(() => setbuttonCheck(!buttonCheck))} checked={buttonCheck} type='checkbox' />
                         <p>Button</p>
                     </div>
                 </div>
+                <div onClick={(() => addFunction())}>
 
-                <Button children={'Add'} styles={{
-                    marginLeft:'auto'
-                }}/>
+                    <Button children={'Add'} styles={{
+                        marginLeft: 'auto'
+                    }} />
+                </div>
             </div>
         </>
     )
