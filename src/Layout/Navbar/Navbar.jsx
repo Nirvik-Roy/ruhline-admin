4420a90bd3236667ea9e41573c62357582e3df5e
Nirvik Roy <nirvikroy802@gmail.com>
2026-04-26 00:22:53 +0530

    logout done

--- src/Layout/Navbar/Navbar.jsx
+++ src/Layout/Navbar/Navbar.jsx
@@ -11,10 +11,12 @@ import { AuthlogOut } from '../../Store/Slices/Loginslice/AuthSlice'
 import Loaders from '../../Components/Loaders/Loaders.jsx'
 import { useNavigate } from 'react-router-dom'
 import { getUserDetails } from '../../utils/user.js'
+import DeleteModal from '../../Components/DeleteModal/DeleteModal.jsx'
 const Navbar = () => {
     const [dropdown, setdropdown] = useState(false);
     const [userData, setuserData] = useState();
-    const [userLoading, setuserLoading] = useState()
+    const [userLoading, setuserLoading] = useState();
+    const [logoutmodal, setlogoutModal] = useState()
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const { isLoading, isLogin } = useSelector(state => state.auth)
@@ -46,7 +48,7 @@ const Navbar = () => {
             const result = await getUserDetails();
             setuserData(result)
         } catch (err) {
-            console.log('User data fethced failed',err)
+            console.log('User data fethced failed', err)
         } finally {
             setuserLoading(false)
         }
@@ -57,38 +59,39 @@ const Navbar = () => {
     }, [])
     return (
         <>
-            {(isLoading || userLoading) && <Loaders />}
+            {logoutmodal && <DeleteModal loading={isLoading} onClick={LogoutFunc} setdeleteModal={setlogoutModal} title={'Logout'} details={'Do you really want to logout?'} />}
+            {/* {(isLoading || userLoading) && <Loaders />} */}
             {modalOpen.password && <ChangePasswordModal modalFunction={modalFunction} />}
             {modalOpen.profile && <ChangeProfileModal userData={userData} userDataFetch={userDataFetch} modalFunction={modalFunction} />}
             <div className='navbar_wrapper'>
                 <div className='container navbar_content_wrapper'>
                     <img src={logo} />
                     <div className='navbar_logout_wrapper'>
-                    <div style={{
-                        display:'flex',
-                        justifyContent:'flex-end',
-                        alignItems:'center',
-                        gap:'10px',
-                        width:'fit-content'
-                    }}>
-                        <div>
-                            <h5 style={{
-                                textAlign:'right'
-                            }}>{userData?.first_name && userData?.first_name} {userData?.last_name && userData?.last_name}</h5>
-                            <small style={{
-                                fontSize:'0.7rem'
-                            }}>{userData?.email && userData?.email}</small>
+                        <div style={{
+                            display: 'flex',
+                            justifyContent: 'flex-end',
+                            alignItems: 'center',
+                            gap: '10px',
+                            width: 'fit-content'
+                        }}>
+                            {userLoading ? 'Loading..' : <div>
+                                <h5 style={{
+                                    textAlign: 'right'
+                                }}>{userData?.first_name && userData?.first_name} {userData?.last_name && userData?.last_name}</h5>
+                                <small style={{
+                                    fontSize: '0.7rem'
+                                }}>{userData?.email && userData?.email}</small>
+                            </div>}
+                            <img onClick={(() => { setdropdown(!dropdown) })} style={{
+                                backgroundColor: userData?.profile_photo && 'transparent',
+                                padding: userData?.profile_photo && '0px',
+                                width: userData?.profile_photo && '35px',
+                                height: userData?.profile_photo && '35px',
+                                borderRadius: userData?.profile_photo && '50%',
+                                objectFit: userData?.profile_photo && 'cover',
+                            }} src={userData?.profile_photo ? userData?.profile_photo : user} />
                         </div>
-                        <img onClick={(() => { setdropdown(!dropdown) })} style={{
-                            backgroundColor: userData?.profile_photo && 'transparent',
-                            padding: userData?.profile_photo && '0px',
-                            width: userData?.profile_photo && '35px',
-                            height: userData?.profile_photo && '35px',
-                            borderRadius: userData?.profile_photo && '50%',
-                            objectFit: userData?.profile_photo && 'cover',
-                        }} src={userData?.profile_photo ? userData?.profile_photo : user} />
-                    </div>
-                        <img onClick={(() => LogoutFunc())} src={logout} />
+                        <img onClick={(() => setlogoutModal(true))} src={logout} />
                         {dropdown && <div className='user_dropdown'>
                             <div className='user_square'></div>
                             <p onClick={(() => modalFunction(2))}>Update Name And Profile Picture</p>
